98c2f38b1d8285eddca39d26d888691e3ec7e8a6
ROBERTO IRIGOYEN L-BAISSON <ririlop@mdef.es>
2024-05-16 15:50:45 +0200

    Mockear datos en el state

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,23 @@
 export default () => ({
   isLoading: true,
-  entries: [1, 2, 3, 4, 5],
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus beatae perferendis voluptatum delectus dolor inventore maiores consectetur facilis natus voluptatibus explicabo alias laborum in, voluptates consequatur! Id facilis inventore laborum.",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam esse iusto corrupti placeat doloribus vero dolores ipsum distinctio sunt provident, sit delectus assumenda, adipisci perspiciatis inventore beatae. Velit, aspernatur facilis? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere animi tenetur est unde excepturi aliquid et accusamus. Nesciunt, quasi veritatis atque dolore, saepe molestiae inventore rem animi tempora, esse eligendi!",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 98765,
+      date: new Date().toDateString(),
+      text: "it. Ullam esse iusto corrupti placeat doloribus vero dolores ipsum distinctio sunt provident, sit delectus assumenda, adipisci perspiciatis inventore beatae. Velit, aspernatur facilis? Lorem ips",
+      picture: null,
+    },
+  ],
 });
