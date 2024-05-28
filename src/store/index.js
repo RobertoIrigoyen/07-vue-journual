2320514298b8a641ae0ea1ff6b27d937573e873a
ROBERTO IRIGOYEN L-BAISSON <ririlop@mdef.es>
2024-05-28 09:32:49 +0200

    Estructuracion de vuex auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 
 import journalModule from "@/modules/daybook/store/journal";
+import authModule from "@/modules/auth/store/auth";
 
 const store = createStore({
   modules: {
     journal: journalModule,
+    auth: authModule,
   },
 });
 
