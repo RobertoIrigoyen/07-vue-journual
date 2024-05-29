102aca2b60d9a0eda73f455cf2ce230f5b8551a4
ROBERTO IRIGOYEN L-BAISSON <ririlop@mdef.es>
2024-05-29 11:58:44 +0200

    Redireccion login

--- src/modules/auth/composables/useAuth.js
+++ src/modules/auth/composables/useAuth.js
@@ -3,13 +3,19 @@ import { useStore } from "vuex";
 const useAuth = () => {
   const store = useStore();
 
-  const createUser = async( user ) => {
-    const resp = await store.dispatch('auth/createUser', user )
-    return resp
-}
+  const createUser = async (user) => {
+    const resp = await store.dispatch("auth/createUser", user);
+    return resp;
+  };
+
+  const loginUser = async (user) => {
+    const resp = await store.dispatch("auth/signInUser", user);
+    return resp;
+  };
 
   return {
     createUser,
+    loginUser
   };
 };
 
